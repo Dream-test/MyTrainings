0e705029defb05c6550f8e3fe6bd369596153e00
Dream-test <stsiapkou@gmail.com>
2024-11-22 16:37:29 +0100

    develop PhoneBook class

--- src/main/train/phonebook/PhoneBook.java
+++ src/main/train/phonebook/PhoneBook.java
@@ -1,6 +1,7 @@
 package train.phonebook;
 
 import java.util.Scanner;
+import java.util.Set;
 
 import static train.phonebook.Person.findPerson;
 import static train.phonebook.Person.initBD;
@@ -12,22 +13,32 @@ public class PhoneBook {
 
         initBD();
 
-        System.out.print("Для поиcка по имени введите 1, по номеру 2, email 3: ");
-        String searchType = in.nextLine();
+        for (;;) {
 
-        System.out.print("Введите поисковое значение: ");
-        String searchString = in.nextLine();
+            System.out.print("Для поиcка по имени введите 1, по номеру 2, email 3, для выхода из справочника 'стоп': ");
+            String searchType = in.nextLine();
+            Set<String> stopWords = Set.of("стоп", "stop");
+            if (stopWords.contains(searchType.toLowerCase())) {
+                System.out.println("Справочник закрыт, работа завершена");
+                break;
+            } else if (!searchType.matches("[1-3]")) {
+                System.out.println("Тип поиска не опознан, должен быть 1, 2 или 3");
+            } else {
+                System.out.print("Введите поисковое значение: ");
+                String searchString = in.nextLine();
 
-        findPerson(searchType, searchString);
+                findPerson(searchType, searchString);
+            }
+        }
 
     }
 }
 
 class Person {
-    private String name;
-    private String phone;
-    private String email;
-    private static Person[] persons = new Person[10];
+    private final String name;
+    private final String phone;
+    private final String email;
+    private final static Person[] persons = new Person[10];
 
     public Person(String name, String phone, String email) {
         this.name = name;
@@ -67,23 +78,66 @@ class Person {
         switch (searchType) {
             case "1":
                 //по имени
-                System.out.println(findName(searchString, persons));
+                System.out.println(findByName(searchString, persons));
                 break;
             case "2":
                 //по телефону
+                System.out.println(findByPhoneNumber(searchString, persons));
                 break;
             case "3":
                 // по почте
+                System.out.println(findByMail(searchString, persons));
                 break;
             default:
                 break;
         }
     }
 
-    public static Person findName(String name, Person[] persons) {
+    public static Person findByName(String name, Person[] persons) {
+        if (!(name.matches("[А-ЯЁа-яё\\sa-zA-Z]*")) || name.isEmpty()) {
+            System.out.println("Имя должно состоять из букв");
+            return null;
+        }
+        String nameToFind = name
+                .replaceAll("\\s", "")
+                .toLowerCase();
         for (Person person : persons) {
             if (person == null) continue;
-            if (person.getName().equals(name)) {
+            if (person.getName().toLowerCase().matches(nameToFind)) {
+                return person;
+            }
+        }
+        System.out.println("нет такого человека");
+        return null;
+    }
+
+    public static Person findByPhoneNumber(String phone, Person[] persons) {
+        String phoneNumber = phone
+                .replaceAll("[()\\s.+,]", "")
+                .trim();
+        if (phoneNumber.isEmpty()) {
+            System.out.println("Не корректный номер телефона");
+            return null;
+        }
+
+        for (Person person : persons) {
+            if (person == null) continue;
+            if (person.getPhone().replaceAll("[()\\s.+,]", "").matches(phoneNumber)) {
+                return person;
+            }
+        }
+        System.out.println("нет такого человека");
+        return null;
+    }
+
+    public static Person findByMail(String email, Person[] persons) {
+        if (!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+            System.out.println("Не корректный e-mail");
+            return null;
+        }
+        for (Person person : persons) {
+            if (person == null) continue;
+            if (person.getEmail().equalsIgnoreCase(email)) {
                 return person;
             }
         }
